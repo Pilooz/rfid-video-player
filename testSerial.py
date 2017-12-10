c8874a3b080720501f491f4b884c83fa12eaacce
Pierre-Gilles Levallois <pierregilles.levallois@gmail.com>
2017-12-10 18:32:00 +0100

    Class MediaAssoc that deals with the association of tags with media

--- testSerial.py
+++ testSerial.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 
 import serial, time, json
+from MediaAssoc import MediaAssoc
 
 # Loading media database
 videoDB = json.load(open('data/videolist.json'))
+
+# Init of the mediaAssoc Class
+db = MediaAssoc(videoDB)
 
 # initialization and open the serial port
 ser = serial.Serial()
@@ -27,28 +31,7 @@ def extract_reader(line):
   if (line.find(patterns[0]) > 0 and line.find(patterns[1]) > -1):
     return line.split(patterns[0], 1)[1].split(patterns[1])[0]
   return None
-
-def DBselect(searched_tag):
-  for elt in videoDB["tags"]:
-    # see if we need to wait for another
-    if(elt["tag"][0] == searched_tag):
-      print ("Found video '{}'".format(elt["media"]))
-      return elt["media"]
-  return None
   
-
-  """
-  Note : 
-  On a besoin d'une classe "mediaAssoc"
-   - qui comporte la BDD
-   - les methodes de recherche
-   - les attributs
-      - DB
-      - Wait  (True lorsque qu'on doit attendre une autre lecture avant de jouer un media)
-      - media (renseigne lorsque que le media associe a ete trouve.)
-
-  """
-
 
 ##############################################################################
 #   M A I N 
@@ -84,17 +67,21 @@ if ser.isOpen():
 
           # 1. if tag is not NONE, seek media database to see if a meddia is associated
           if (tag != None):
-            media = DBselect(tag)
-            if ( media != None ):
-              # 1.1. Media Found
-              print(media)
+            if (db.needWaiting(tag)):
+              print("Waiting for another tag bites the dust...")
             else:
-              # 1.2. Media not found
-              print("No media associated ! ")
+              media = db.getFile(tag)
+              print("------------> {}".format(media))
+              if ( media != None ):
+                # 1.1. Media Found
+                print(media)
+              else:
+                # 1.2. Media not found
+                print("No media associated ! ")
 
-              # 2. See if we need to wait for another tag
-                # 2.1. No need to wait : print the TagId and say we have to add media
-                # 2.2. waiting for another tag, so say it with a little message
+                # 2. See if we need to wait for another tag
+                  # 2.1. No need to wait : print the TagId and say we have to add media
+                  # 2.2. waiting for another tag, so say it with a little message
 
 
           else:
