9cbff68d666e71696471441b03aee0f633db922b
Pierre-Gilles Levallois <pgl@erasme.org>
2018-04-26 15:00:03 +0200

    refactor media manipulation in a module
    refactor media manipulation in a module

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var CONFIG        = require('./config/config.js');
-var in_array      = require('in_array');
 var app 			    = require('express')();
 var express			  = require('express');
 var router        = express.Router();
@@ -15,10 +14,8 @@ var fs            = require('fs');
 // Rfid parsing functions
 var rfid          = require('./lib/rfid.js');
 
-// Databases
-var db_keywords   = require('./data/keywords.js');
-var db_media      = require('./data/media.js');
-
+// Media DB functions
+var mediaDB       = require('./lib/mediaDB.js');
 
 // RFID Data structure
 var lastRfidData = { tag: "", reader: "" };
@@ -42,80 +39,6 @@ const mediaNotFoundMedia = { uri: CONFIG.app.mediaPath + "/messages/mediaNotFoun
 const noTagAssocMedia = { uri: CONFIG.app.mediaPath + "/messages/noTagAssociation.mp4", loop: "off", autoplay: "on", controls: "off", status: "noTagAssociation", tag: "" };
 const searchingMedia = { uri: CONFIG.app.mediaPath + "/messages/searching.mp4", loop: "off", autoplay: "on", controls: "off", status: "searching", tag: "" };
 
-console.log(db_keywords.keywordslist.length + " keywords in database.");
-console.log(db_media.medialist.length + " medias in database.");
-
-//
-// Sorting unique in an array
-//
-function sort_unique(arr) {
-  if (arr.length === 0) return arr;
-  arr = arr.sort(function (a, b) { return a*1 - b*1; });
-  var ret = [arr[0]];
-  for (var i = 1; i < arr.length; i++) { //Start loop at 1: arr[0] can never be a duplicate
-    if (arr[i-1] !== arr[i]) {
-      ret.push(arr[i]);
-    }
-  }
-  return ret;
-}
-
-// Building a keyword list with rfid Tag
-function buildKeywordList(rfidTag){
-  // Builded keyword list from RFID tag
-  var keyList = [];
-  var index = 0;
-  // Parcours tous les mots clés
-  db_keywords.keywordslist.forEach(function(k){
-    // Pour chaque mot clé, parcours tous les codes associés
-    k.codes.forEach(function(c){
-      if (c == rfidTag) {
-        // Ajout du mot clé à la liste
-        keyList[index] = k.keyword;
-        index++;
-        return false; // Easy way to break loop !
-      }
-    });
-  });
-  return sort_unique(keyList);
-}
-
-// Building media list
-function buildMediaList(rfidTag){
-  // Builded media list from keywordList
-  var mediaCollection = [];
-  var index = 0;
-  // First Building keyword List 
-  var keys = buildKeywordList(rfidTag);
-  console.log(keys);  
-
-  // Parcours des medias
-  db_media.medialist.forEach(function(m){
-    // Pour chaque mots-clé
-    m.keywords.forEach(function(k){
-      // Si les mots clé associés au media sont dans la listes de mots clé générée
-      // On ajoute le média
-      if ( in_array(k, keys) ) {
-        mediaCollection[index] = CONFIG.app.mediaPath + "/" + m.media;
-        index++;
-        return false;
-      }
-    });
-  });
-  var col = sort_unique(mediaCollection);
-  console.log(col); 
-  return col;
-  // return sort_unique(mediaCollection);
-}
-
-// Choosing a media randomly in the list
-function chooseMedia(arr) {
-  var i = Math.floor(Math.random()*arr.length);
-  var m = arr[i];
-  console.log("Choosen #" + i + " : " + m);
-  return m;
-  // return arr[Math.floor(Math.random()*arr.length)];
-}
 //------------------------------------------------------------------------
 // Init Socket to transmit Serial data to HTTP client
 //------------------------------------------------------------------------
@@ -156,13 +79,13 @@ function sendEachTime() {
       }
       setTimeout(function() {
         var medias = [];
-        medias = buildMediaList(rfidData.tag);
+        medias = mediaDB.buildMediaList(rfidData.tag, CONFIG.app.mediaPath);
         // If media array if empty, the RFID tag was not associated
         if ( medias.length == 0 ) {
           noTagAssocMedia.tag = rfidData.tag;
           io.emit('server.play-media', noTagAssocMedia);
         } else {
-          mediaFile = { uri: chooseMedia(medias), loop: "off", autoplay: "on", controls: "on", status: "content", tag: rfidData.tag };
+          mediaFile = { uri: mediaDB.chooseMedia(medias), loop: "off", autoplay: "on", controls: "on", status: "content", tag: rfidData.tag };
           // Verifying that file exists
           if (fs.existsSync(path.join(__dirname, mediaFile.uri))) { 
             io.emit('server.play-media', mediaFile);
