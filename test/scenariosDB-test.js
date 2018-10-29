23e4fd7e2fa23f824d79069da1c984572e4d0ef8
Pierre-Gilles Levallois <pgl@erasme.org>
2018-10-29 22:48:51 +0100

    add unit test for scenariosDB lib

--- test/scenariosDB-test.js
+++ test/scenariosDB-test.js
@@ -48,8 +48,61 @@ describe('#_buildScenarList', function() {
   });
   
   it('tests array content', function() {
-  	a1.scenarId.should.be.equal('sceanrio1');
-  	a4.scenarId.should.be.equal("sceanrio1");
+  	a1[0].scenarId.should.be.equal('scenario1');
+  	a4[0].scenarId.should.be.equal("scenario1");
   });
+
+  var sc = a1[0].steps[0];
+  ["stepId", "title", "icon", "template", "background", "choice", "medias", "text", "transitions"].forEach(function(p){
+    it('scenario1.steps[0] returned by "_buildScenarList" should have property "'+p+'"', function() {
+      sc.should.have.property(p);
+    });
+  });
+
+  ['id', 'condition', 'duration'].forEach(function(p){
+    it('scenario1.steps[0].transitions[0] returned by "_buildScenarList" should have property "'+p+'"', function() {
+      sc.transitions[0].should.have.property(p);
+    });
+  });
+
+
+  it('returns value "step-1"', function() {
+    sc.stepId.should.be.equal("step-1");
+  });
+  it('returns value "Attributes validation"', function() {
+    sc.title.should.be.equal("Attributes validation");
+  });
+  it('returns value "icon.png"', function() {
+    sc.icon.should.be.equal("icon.png");
+  });
+  it('returns value "video"', function() {
+    sc.template.should.be.equal("video");
+  });
+  it('returns value "image1.png"', function() {
+    sc.background.should.be.equal("image1.png");
+  });
+  it('returns value "Choice 1"', function() {
+    sc.choice[0].should.be.equal("Choice 1");
+  });
+  it('returns value "image1.png"', function() {
+    sc.medias[0].should.be.equal("image1.png");
+  });
+  it('returns value "text1"', function() {
+    sc.text.should.be.equal("text1");
+  });
+  it('returns type "array"', function() {
+    sc.transitions.should.be.an('array');
+  });
+
+  it('returns value "step-2"', function() {
+    sc.transitions[0].id.should.be.equal("step-2");
+  });
+  it('returns value "endMedia"', function() {
+    sc.transitions[0].condition.should.be.equal("endMedia");
+  });
+  it('returns "2000"', function() {
+    sc.transitions[0].duration.should.be.equal(2000);
+  });
+
 
 });
